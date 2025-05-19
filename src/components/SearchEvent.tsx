b67f6951716a4fe6046a7c7d4f851179230c5136
ItsVicky72e <bojjavikranth090@gmail.com>
2025-05-19 12:17:34 +0530

    Update

--- src/components/SearchEvent.tsx
+++ src/components/SearchEvent.tsx
@@ -1,59 +1,65 @@
-import React, { useState } from 'react'
-import NavBar from './NavBar'
+import React, { useState } from 'react';
+import NavBar from './NavBar';
 import axios from 'axios';
 
 export default function SearchEvent() {
+  const [eventName, setEventName] = useState('');
+  const [eventData, setEventData] = useState<any>(null);
 
-  const [eventName, setEventName] = useState();
-
-  const searchItem = (e: FormDataEvent) => {
+  const searchItem = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("item--->" + eventName);
-    axios.get("http://localhost:8080/cse/dept/sports/search?eventName="+eventName)
-    .then(
-      (res:any)=>{
-        console.log("res--->"+res.data);
-        console.log("res   name--->"+res.data.eventDate);
-        
-      }
-    )
+    axios
+      .get(`http://localhost:8080/cse/dept/sports/search?eventName=${eventName}`)
+      .then((res: any) => {
+        console.log("res--->", res.data);
+        setEventData(res.data); // Save data to state
+      })
+      .catch(err => {
+        console.error("Error fetching event data:", err);
+      });
+  };
 
-  }
-
-  const getEventName = (e: any) => {
+  const getEventName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEventName(e.target.value);
-  }
+  };
+
   return (
     <div>
       <NavBar />
       <div>
-
         <form onSubmit={searchItem}>
-          <input type="text"
-            onChange={getEventName} />
+          <input type="text" onChange={getEventName} />
           <input type="submit" />
-
         </form>
 
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">EventName</th>
-      <th scope="col">EventDate</th>
-      <th scope="col">Sport Name</th>
-      <th scope="col">Sport Type</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"></th>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-   </tbody>
-</table>
-
-      </div></div>
-  )
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Event Name</th>
+              <th scope="col">Event Date</th>
+              <th scope="col">Sport Name</th>
+              <th scope="col">Sport Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {eventData ? (
+              <tr>
+                <td>{eventData.eventName}</td>
+                <td>{eventData.eventDate}</td>
+                <td>{eventData.sportName}</td>
+                <td>{eventData.sportType}</td>
+              </tr>
+            ) : (
+              <tr>
+                <td colSpan={4} style={{ textAlign: 'center' }}>
+                  No event data found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
