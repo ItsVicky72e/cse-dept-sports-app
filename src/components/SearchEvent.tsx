3086bce05ef73f3db12c1a565c6aaac8479fa05b
ItsVicky72e <bojjavikranth090@gmail.com>
2025-05-19 12:03:59 +0530

    Update SearchEvent.tsx

--- src/components/SearchEvent.tsx
+++ src/components/SearchEvent.tsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios';
 
 export default function SearchEvent() {
+
+  const [eventName, setEventName] = useState();
+
+  const searchItem = (e: FormDataEvent) => {
+    e.preventDefault();
+    console.log("item--->" + eventName);
+    axios.get("http://localhost:8080/cse/dept/sports/search?eventName="+eventName)
+    .then(
+      (res:any)=>{
+        console.log("res--->"+res.data);
+        console.log("res   name--->"+res.data.eventDate);
+        
+      }
+    )
+
+  }
+
+  const getEventName = (e: any) => {
+    setEventName(e.target.value);
+  }
   return (
     <div>
       <NavBar />
-      SearchEvent</div>
+      <div>
+
+        <form onSubmit={searchItem}>
+          <input type="text"
+            onChange={getEventName} />
+          <input type="submit" />
+
+        </form>
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">EventName</th>
+      <th scope="col">EventDate</th>
+      <th scope="col">Sport Name</th>
+      <th scope="col">Sport Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+   </tbody>
+</table>
+
+      </div></div>
   )
 }
